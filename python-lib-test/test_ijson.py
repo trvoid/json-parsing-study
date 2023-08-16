9df893201f7ca11c52a139a28d762ffa497bd164
Wayne Jeong <wayne.jeong.wj@gmail.com>
2023-08-16 20:20:18 +0900

    ijson.items()

--- python-lib-test/test_ijson.py
+++ python-lib-test/test_ijson.py
@@ -16,7 +16,58 @@ import ijson
 def print_memory_usage(message):
     print(f'*** MEMORY USAGE : {psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2:.1f} MB ({message}) ***')
 
-def test_ijson_initial_part(input_filepath):
+def test_ijson_items_initial_part(input_filepath):
+    print('##### Test ijson.items() : Get the initial LOT_NUM value #####')
+    print_memory_usage('begin')
+    start_time = time.time()
+
+    lot_num_value = None
+    lot_num_count = 0
+
+    with open(input_filepath, 'r') as f:
+        feature_items = ijson.items(f, 'features.item')
+
+        print_memory_usage('after ijson.items(f)')
+
+        for feature_item in feature_items:
+            lot_num_value = feature_item['properties']['LOT_NUM']
+            lot_num_count += 1
+            break
+
+    elapsed_time = time.time() - start_time
+    print(f'Elapsed time: {elapsed_time:0.2f} seconds')
+
+    print_memory_usage('processing done')
+
+    print(f'lot_num_value: {lot_num_value}')
+    print(f'lot_num_count: {lot_num_count}')
+
+def test_ijson_items_last_part(input_filepath):
+    print('##### Test ijson.items() : Get the last LOT_NUM value #####')
+    print_memory_usage('begin')
+    start_time = time.time()
+
+    lot_num_value = None
+    lot_num_count = 0
+
+    with open(input_filepath, 'r') as f:
+        feature_items = ijson.items(f, 'features.item')
+
+        print_memory_usage('after ijson.items(f)')
+
+        for feature_item in feature_items:
+            lot_num_value = feature_item['properties']['LOT_NUM']
+            lot_num_count += 1
+
+    elapsed_time = time.time() - start_time
+    print(f'Elapsed time: {elapsed_time:0.2f} seconds')
+
+    print_memory_usage('processing done')
+
+    print(f'lot_num_value: {lot_num_value}')
+    print(f'lot_num_count: {lot_num_count}')
+
+def test_ijson_parse_initial_part(input_filepath):
     print('##### Test ijson.parse() : Get the initial LOT_NUM value #####')
     print_memory_usage('begin')
     start_time = time.time()
@@ -43,7 +94,7 @@ def test_ijson_initial_part(input_filepath):
     print(f'lot_num_value: {lot_num_value}')
     print(f'lot_num_count: {lot_num_count}')
 
-def test_ijson_last_part(input_filepath):
+def test_ijson_parse_last_part(input_filepath):
     print('##### Test ijson.parse() : Get the last LOT_NUM value #####')
     print_memory_usage('begin')
     start_time = time.time()
@@ -80,8 +131,11 @@ def main():
 
     input_filepath = sys.argv[1]
    
-    #test_ijson_initial_part(input_filepath)
-    test_ijson_last_part(input_filepath)
+    #test_ijson_items_initial_part(input_filepath)
+    #test_ijson_items_last_part(input_filepath)
+
+    #test_ijson_parse_initial_part(input_filepath)
+    test_ijson_parse_last_part(input_filepath)
 
 if __name__ == '__main__':
     try:
